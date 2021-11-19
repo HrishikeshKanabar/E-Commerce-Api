86f29fe978a73b3c27ca3048574ec0fc04c07ff9
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-18 20:12:35 -0500

    Fixing alias and product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,48 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'tags'
+      }
+    ]
+  }).then(allProd => {
+      res.json(allProd);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['id', 'tag_name'],
+      through: ProductTag,
+      as: 'tags'
+    }
+    ]
+  })
+    .then(getProdById => {
+      
+      res.json(getProdById);
+    })
 });
 
 // create new product
@@ -90,7 +126,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  // delete one product by its `id` value.
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(deletedProds => {
+      res.json(deletedProds);
+    })
 });
 
 module.exports = router;
